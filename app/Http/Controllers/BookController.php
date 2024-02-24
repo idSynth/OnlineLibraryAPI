36b7948fb78a05c81439a512e24ef8c62bfa20ca
idSynth <idsynthdev@gmail.com>
2024-02-24 03:47:43 +0200

    - Added genres and authors functionality - Added book_genres and book_authors functionality

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Authors;
 use App\Models\Book_Authors;
 use App\Models\Book_Genres;
 use App\Models\Books;
+use App\Models\Genres;
 use App\Models\User_Favorites;
 use Illuminate\Http\Request;
 use Illuminate\Database;
@@ -23,12 +25,73 @@ class BookController extends Controller
     public function add(Request $request)
     {
         $this->validate($request, [
-            'name' => 'required|string'
+            'name' => 'required|string',
+            'genres' => 'required|string',
+            'authors' => 'required|string'
         ]);
 
+        # Handle genres
+        $genres = explode(',', $request->input('genres'));
+        foreach ($genres as $genre) {
+            $genre = trim($genre); // Remove any whitespace
 
-        //TODO: Add genres and authors creation
+            // Convert genre to lowercase and make the first letter uppercase
+            $genre = ucfirst(strtolower($genre));
+
+            // Check if the genre name is valid
+            if (!preg_match('/^[a-zA-Z\s]+$/', $genre)) {
+                return response()->json(['error' => 'Invalid genre name: ' . $genre], 400);
+            }
+
+            // Check if the genre exists in the Genres table
+            $genreModel = Genres::where('genre', $genre)->first();
+
+            // If the genre doesn't exist, create a new one
+            if (!$genreModel) {
+                $genreModel = Genres::create(['genre' => $genre]);
+            };
+        }
+
+        # Handle authors
+        $authors = explode(',', $request->input('authors'));
+        foreach ($authors as $author) {
+            $author = trim($author); // Remove any whitespace
+
+            // Check if the genre name is valid
+            if (!preg_match('/^[a-zA-Z\s\.]+$/', $author)) {
+                return response()->json(['error' => 'Invalid author name: ' . $author], 400);
+            }
+
+            // Check if the genre exists in the Genres table
+            $authorModel = Authors::where('name', $author)->first();
+
+            // If the genre doesn't exist, create a new one
+            if (!$authorModel) {
+                $authorModel = Authors::create(['name' => $author]);
+            };
+        }
+
+        # Create book
         $book = Books::create(['name' => $request->input('name')]);
+
+        # Add book to Book_Genres table
+        foreach ($genres as $genre){
+            $genre = trim($genre); // Remove any whitespace
+            // Check if the genre exists in the Genres table
+            $genreModel = Genres::where('genre', $genre)->first();
+
+            Book_Genres::create(['book_id' => $book->id, 'genre_id' => $genreModel->id]);
+        }
+
+        # Add book to Book_Authors table
+        foreach ($authors as $author){
+            $author = trim($author); // Remove any whitespace
+            // Check if the genre exists in the Genres table
+            $authorModel = Authors::where('name', $author)->first();
+
+            Book_Authors::create(['book_id' => $book->id, 'author_id' => $authorModel->id]);
+        }
+
 
         return response()->json(['book' => $book], 201);
     }
